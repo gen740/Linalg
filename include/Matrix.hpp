60d199a7012ca6fc95f574b5cbf02aaef3a00ead
gen740 <gen740@gmail.com>
2022-06-21 23:18:30 +0900

    Initial Commit

---
+++ include/Matrix.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <lapacke.h>
+
+#include <array>
+#include <exception>
+#include <iosfwd>
+#include <vector>
+
+namespace Linalg {
+
+class Vector {
+ public:
+  Vector(int size) : m_SIZE(size), m_data(size) {}
+  Vector() : m_SIZE(0), m_data(0) {}
+
+  int size() { return m_SIZE; };
+  double &operator()(int n) {
+    if (0 <= n && n < m_SIZE) {
+      return m_data[n];
+    } else {
+      throw std::runtime_error("Index out of range");
+    }
+  }
+  double operator()(int n) const {
+    if (0 <= n && n < m_SIZE) {
+      return m_data[n];
+    } else {
+      throw std::runtime_error("Index out of range");
+    }
+  }
+  friend std::ostream &operator<<(std::ostream &os, const Vector &frac);
+
+ private:
+  std::vector<double> m_data;
+  int m_SIZE;
+};
+
+struct LU_status {
+  int status;
+  std::vector<int> ipiv;
+  operator int() const { return status; }
+  LU_status(int size) : ipiv(size) {}
+};
+
+class Matrix {
+ public:
+  Matrix(int col, int row) : m_COL(col), m_ROW(row), m_data(col * row) {}
+  Matrix() : m_COL(0), m_ROW(0), m_data(0) {}
+  Matrix(const Matrix &mat)
+      : m_COL(mat.m_COL), m_ROW(mat.m_ROW), m_data(mat.m_data) {}
+
+  // {col, row}
+  std::array<int, 2> shape() const { return {m_COL, m_ROW}; }
+
+  LU_status lu() {
+    static LU_status status(m_ROW);
+    std::vector<int> ipiv;
+    status.status = LAPACKE_dgetrf(LAPACK_COL_MAJOR, m_COL, m_ROW,
+                                   m_data.data(), m_COL, status.ipiv.data());
+    return status;
+  }
+
+  double det() {
+    double det(1.);
+    Matrix m = *this;
+    if (m.m_ROW != m.m_COL) {
+      throw std::runtime_error("Matrix is not squre");
+    }
+    auto status = m.lu();
+    for (int i = 0; i < m.m_COL; ++i) {
+      det *= m(i, i);
+      if (status.ipiv.at(i) != i + 1) {
+        det *= -1;
+      }
+    }
+    return det;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Matrix &frac);
+  double &operator()(int col, int row) {
+    if (col < m_COL && row < m_ROW) {
+      return m_data[col * m_ROW + row];
+    } else {
+      throw std::runtime_error("Index out of range");
+    }
+  }
+
+  double operator()(int col, int row) const {
+    if (col < m_COL && row < m_ROW) {
+      return m_data[col * m_ROW + row];
+    } else {
+      throw std::runtime_error("Index out of range");
+    }
+  }
+
+ private:
+  std::vector<double> m_data;
+  int m_COL, m_ROW;
+  int m_precision{3};
+};
+
+}  // namespace Linalg
---
+++ src/Matrix.cpp
@@ -0,0 +1,35 @@
+#include "Matrix.hpp"
+
+#include <iomanip>
+
+namespace Linalg {
+
+std::ostream &operator<<(std::ostream &os, const Vector &vec) {
+  os << "Vector n=" << vec.m_SIZE << "\n";
+  for (int i = 0; i < vec.m_SIZE; ++i) {
+    os << "\t" << vec(i);
+  }
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const Matrix &mat) {
+  os << "Matrix " << mat.m_COL << "×" << mat.m_ROW << "\n";
+  // os << std::setprecision(mat.m_precision);
+  for (int i = 0; i < mat.m_COL; ++i) {
+    os << "\t";
+    for (int j = 0; j < mat.m_ROW; ++j) {
+      if (j == mat.m_ROW - 1) {
+        if (i == mat.m_COL - 1) {
+          os << mat(i, j);
+          break;
+        }
+        os << mat(i, j) << "\n";
+      } else {
+        os << mat(i, j) << "\t";
+      }
+    }
+  }
+  return os;
+}
+
+}  // namespace Linalg
